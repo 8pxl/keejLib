2256f487c95ddfb3f59a87be981819e5218a8a19
8pxl <56054380+8pxl@users.noreply.github.com>
2023-05-05 22:27:31 -0700

    Update lib.hpp

--- src/keejLib/lib.hpp
+++ src/keejLib/lib.hpp
@@ -1,10 +1,19 @@
 #pragma once
+
+//include all lib files
 #include "../include/keejLib/lib.h"
+
+//movement
 #include "movement/1dMotionProfiling.hpp"
 #include "movement/1dPid.hpp"
 #include "movement/2dPid.hpp"
+
+//misc wrappers
 #include "chassis.hpp"
 #include "controller.hpp"
 #include "groups.hpp"
-#include "odom.hpp"
-#include "util.hpp"
+
+//util
+#include "util/odom.hpp"
+#include "util/util.hpp"
+#include "util/bezier.hpp"
