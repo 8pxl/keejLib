92c2b5a0f78e019b38dc9d820697b60db27a3589
8pxl <56054380+8pxl@users.noreply.github.com>
2024-05-23 12:16:20 -0700

    removed code from header

--- lib/include/keejlib/chassis.h
+++ lib/include/keejlib/chassis.h
@@ -1,25 +1,13 @@
 #include "main.h"
 #include "pros/motors.hpp"
-#include <array>
 #include "units.h"
-
-// using namespace units::literals;
-using namespace units::angular_velocity;
 
 namespace keejLib {
     class DriveTrain : pros::MotorGroup {
         private:
-            std::vector<std::int8_t>* concat(const std::vector<std::int8_t>& left_ports, const std::vector<std::int8_t>& right_ports);
+            std::vector<std::int8_t> concat(const std::vector<std::int8_t>& left_ports, const std::vector<std::int8_t>& right_ports);
         public:
-            void spinVolts(int left, int right) {
-                _motor_group_mutex.take();
-                int half = _motor_count / 2;
-                for (int i = 0; i < half; i++) {
-                    _motors[i].move_voltage(left);
-                    _motors[i + half].move_voltage(right);
-                }
-                _motor_group_mutex.give();
-            }
+            void spinVolts(int left, int right);
             DriveTrain(const std::vector<std::int8_t>& left_ports, const std::vector<std::int8_t>& right_ports);
     };
     
