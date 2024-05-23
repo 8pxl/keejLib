616f57f9a1dd106c73cb40fa744a5bab5c133962
8pxl <56054380+8pxl@users.noreply.github.com>
2024-05-23 12:45:15 -0700

    add chassis constants

--- lib/include/keejlib/chassis.h
+++ lib/include/keejlib/chassis.h
@@ -1,18 +1,33 @@
+#include "keejlib/units.h"
 #include "main.h"
 #include "pros/motors.hpp"
 #include "units.h"
 
+using namespace units::length;
+using namespace units::angular_velocity;
+
 namespace keejLib {
-    class DriveTrain : pros::MotorGroup {
+    struct ChassConstants {
+        inch_t trackWidth;
+        inch_t trackDia;
+        revolutions_per_minute_t rpm;
+    };
+    
+    class DriveTrain : public pros::MotorGroup {
         private:
             std::vector<std::int8_t> concat(const std::vector<std::int8_t>& left_ports, const std::vector<std::int8_t>& right_ports);
         public:
             void spinVolts(int left, int right);
             DriveTrain(const std::vector<std::int8_t>& left_ports, const std::vector<std::int8_t>& right_ports);
+            void spinLeft(int volts);
+            void spinRight(int volts);
     };
     
     class Chassis {
+        private: 
+            DriveTrain dt;
+            ChassConstants constants;
         public:
-            
+            Chassis(DriveTrain dt, ChassConstants constats);
     };
 }
